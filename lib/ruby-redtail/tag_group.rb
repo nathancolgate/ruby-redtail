7a71b2a029700b39b92aca787a8c963e12f14400
Nathan Colgate Clark <nathancolgate@gmail.com>
2014-04-29 14:31:16 -0500

    Cleaning up tag group sub methods

--- lib/ruby-redtail/tag_group.rb
+++ lib/ruby-redtail/tag_group.rb
@@ -18,7 +18,25 @@ module RubyRedtail
     end
     
     def contacts
-      RubyRedtail::TagGroup::Contacts.new(@id,@api_hash)
+      build_contacts_array RubyRedtail::Query.run("taggroups/#{@id}/contacts", @api_hash, "GET")["TagMember_Result"]["TagMembers"]
+    end
+      
+    private
+    
+    def build_contact contact_hash
+      if contact_hash
+        RubyRedtail::Contact.new(contact_hash,@api_hash)
+      else
+        raise RubyRedtail::AuthenticationError
+      end
+    end
+
+    def build_contacts_array contact_hashes
+      if contact_hashes
+        contact_hashes.collect { |contact_hash| self.build_contact contact_hash }
+      else
+        raise RubyRedtail::AuthenticationError
+      end
     end
     
   end
